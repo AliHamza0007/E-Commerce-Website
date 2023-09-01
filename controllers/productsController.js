4473dcd946688bd8a03b748da400dd2795a0ca10
AliHamza0007 <hamzasarwer9@gmail.com>
2023-08-31 21:06:09 -0700

    li change hover

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -235,7 +235,7 @@ export const countProductController = async (req, res) => {
 };
 export const pageProductController = async (req, res) => {
   try {
-    const PerPage = 24;
+    const PerPage = 12;
     const page = req.params.page ? req.params.page : 1;
     const products = await ProductModel.find({})
       .select("-photo")
